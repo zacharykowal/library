69dea7568593f0cf781903ab11fcb8ad6b0f09a2
Zachary Kowal <zacharykowal31@gmail.com>
2025-08-26 15:34:09 -0400

    Write library display function

--- script.js
+++ script.js
@@ -18,3 +18,35 @@ function addBookToLibrary(title, author, pages, isRead) {
     const book = new Book(title, author, pages, isRead);
     myLibrary.push(book);
 }
+
+function addLibraryToDisplay() {
+    for (const book of myLibrary) {
+
+        const newCard = document.createElement("div");
+        newCard.classList.add("card");
+
+        const title = document.createElement("p");
+        const author = document.createElement("p");
+        const pages = document.createElement("p");
+        const isRead = document.createElement("p");
+
+        title.textContent = `Title: ${book.title}`;
+        author.textContent = `Author: ${book.author}`;
+        pages.textContent = `Pages: ${book.pages}`;
+
+        const content = book.isRead ? "Read: Yes" : "Read: No";
+        isRead.textContent = content;
+
+        newCard.append(title, author, pages, isRead);
+
+        const cardDisplay = document.getElementsByClassName("card-display")[0];
+        console.log(cardDisplay);
+        cardDisplay.appendChild(newCard);
+    }
+}
+
+addBookToLibrary("Harry Potter", "JK Rowling", 300, true);
+addBookToLibrary("My Book", "Me", 200, false);
+addBookToLibrary("Your Book", "You", 50, true);
+
+addLibraryToDisplay();
